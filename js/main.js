3adfc7a7c786668234567f67e3d54c4fff13f993
KeSha Smith <103616990+KeShaKreates@users.noreply.github.com>
2022-10-02 22:00:08 -0700

    front end changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-alert('works')
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const chakraName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://chakras-api.herokuapp.com/api/${chakraName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.aka
+    }catch(error){
+        console.log(error)
+    }
+}
