dc357c7ee202e37a7e3ce7a8ffa772677bb805d3
KeSha Smith <103616990+KeShaKreates@users.noreply.github.com>
2022-10-02 22:50:40 -0700

    front end changes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const chakrasName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://chakras-api.herokuapp.com/api/${chakrasName}`)
+        const response = await fetch(`https://chakras-api.herokuapp.com//api/${chakrasName}`)
         const data = await response.json()
 
         console.log(data)
